164bb7d7e5c1db06545e6d25de271c17abc96cdf
Saw Aye Chan <sawayechan@gmail.com>
2016-06-19 21:31:35 +0800

    Second commit

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var http = require('http');
+var port = process.env.PORT || 1337;
+
 var server =http.createServer((req,res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('okay');
 });
 
-server.listen(1337,'127.0.0.1');
+server.listen(port,'127.0.0.1',function () {
+    console.log('Server is running at %s:%s '
+    ,server.address().address
+    ,server.address().port);
+});
