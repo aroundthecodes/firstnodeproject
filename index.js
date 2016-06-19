b1235f5ff7e87f240f9e81c259abb1267eb92c4c
Saw Aye Chan <sawayechan@gmail.com>
2016-06-19 22:43:18 +0800

    Fifth commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 1337;
 
 var server =http.createServer((req,res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('okay');
+    res.end('okay, welcome to nodejs, git, azure :P');
 });
 
 server.listen(port,'127.0.0.1',function () {
